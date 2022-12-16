9af2d7ed2e5f8b3732ff6e11da99da09952c1b9f
Hemanth B <63582474+HemanthB03@users.noreply.github.com>
2022-12-16 22:11:53 +0530

    dfs and bfs

---
+++ csl201-dsa-lab/bfs.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int a[20][20], q[20], visited[20], n, f=-1, r=-1;
+
+void bfs(int v) {
+    int i;
+    for(i=0; i<n; i++) {
+        if(a[v][i]!=0 && visited[i]==0) {
+            r=r+1;
+            q[r]=i;
+            visited[i]=1;
+            printf("V%d, ", i);
+        }
+    }
+    f=f+1;
+    if(f<=r) {
+        bfs(q[f]);
+    }
+}
+
+int main() {
+
+    int v, i, j;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+    for(i=0; i<n; i++) {
+        visited[i]=0;
+    }
+    printf("Enter the adjacency matrix of the graph: \n");
+    for(i=0; i<n; i++) {
+        for(j=0; j<n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter the starting vertex: ");
+    scanf("%d", &v);
+
+    f=0;
+    r=0;
+    q[r]=v;
+
+    printf("BFS traversal is: ");
+    visited[v]=1;
+    printf("V%d, ", v);
+
+    bfs(v);
+
+    return 0;
+}
