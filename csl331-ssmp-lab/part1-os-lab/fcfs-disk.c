58e85752ef5833f58a06c9252632ae3cc5e5099c
Hemanth B <63582474+HemanthB03@users.noreply.github.com>
2023-10-12 22:45:43 +0530

    fcfs-disk

---
+++ csl331-ssmp-lab/part1-os-lab/fcfs-disk.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int ioq[20], i, n, ihead, tot;
+  float seek=0, avgs;
+
+  printf("FCFS Disk Schedulinng\n");
+  printf("Enter the no of request: ");
+  scanf("%d", &n);
+  printf("Enter the initial head position: ");
+  scanf("%d", &ihead);
+  
+  ioq[0]=ihead;
+
+  printf("Enter the I/O queue request\n");
+  for(i=1; i<=n; i++) {
+    scanf("%d", &ioq[i]);
+  }
+  ioq[n+1]=ioq[n];
+  printf("\nThe order of request served is: \n");
+  for(i=0; i<=n; i++) {
+    printf("%d-->", ioq[i]);
+  }
+
+  printf("\n\nstart\tend\tdifference\n");
+  for(i=0; i<n; i++) {
+    tot=abs(ioq[i+1]-ioq[i]);
+    seek+=tot;
+    printf("%d\t%d\t%d\n", ioq[i], ioq[i+1], tot);
+  }
+
+  avgs=seek/n;
+  printf("\nTotal seek operations\t: %.2f\n", seek);
+  printf("Average seek operations\t: %.2f\n", avgs);
+
+  return 0;
+}
